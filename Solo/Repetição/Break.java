064a7769bc0f9d33707a83bb904fd535b5af426c
Cauan71 <126925374+Cauan71@users.noreply.github.com>
2025-02-17 20:00:11 +0000

    @Cauan71

---
+++ Solo/Repetição/Break.java
@@ -0,0 +1,19 @@
+package Solo.Repetição;
+
+public class Break {
+
+    // Imprima o primeiros 25 números , de um dado valor EX: 50
+    public static void main(String[] args) {
+        
+        for (int i = 0; i <= 50; i++) {
+            System.out.println(i);
+
+            if (i == 25) {
+                break;
+                
+            }
+            
+        }
+    }
+    
+}
