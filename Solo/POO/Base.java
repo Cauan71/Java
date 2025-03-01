671ea63a5d927f795db44b5c28797718f40c5dad
Cauan71 <126925374+Cauan71@users.noreply.github.com>
2025-03-01 20:14:33 -0300

    @Cauan71

---
+++ Solo/POO/Base.java
@@ -0,0 +1,12 @@
+public class Base{
+    public static void main(String[] args){
+        Professor professor = new Professor();
+
+        professor.name = "Iuri";
+        professor.idade = 20;
+        professor.sexo = "M";
+
+        
+    }
+    
+}
